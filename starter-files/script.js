c0c948bec32c97bd45e78670df65a859d90e4d7a
David <davidesv69@gmail.com>
2023-01-13 12:16:13 -0600

    Ejercicio 2 Livecoding

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,5 +1,11 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
+let button = document.getElementById("button");
+let answer = document.getElementById("answer");
+const input = document.getElementById("input");
+const error_message = document.getElementById("error");
+
+
 
 /**
  * STEPS:
@@ -11,3 +17,34 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+function fetchAnswer(){
+    if (input.value === ""){
+        error_message.innerHTML = "Por favor ingresa una pregunta";
+        return;
+    }
+error_message.innerHTML = "";
+
+fetch(API_ENDPOINT)
+.then(res => res.json())
+.then(data => {
+    answer.innerHTML = data.answer; 
+    document.getElementById("ball").style.display = "block";
+    setTimeout(() => {
+        input.value = "";
+        answer.innerHTML = "";
+        document.getElementById("ball").style.display = "none";
+    }, 5000)
+})
+.catch(err => console.log(err));
+}
+
+button.addEventListener("click", fetchAnswer);
+
+input.addEventListener("input", () => {
+    if(input.value === ""){
+        button.disabled = true;
+    } else{
+        button.disabled = false;
+    }
+});
